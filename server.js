895b089cc1286071eb193efb6c9b860bad11ade7
miwzerking <0865697@students.dadeschools.net>
2020-12-20 15:50:09 -0500

    first deployment

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+//app.use(helmet());
+app.use(cors());
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(PORT, () => console.log(`listenning on port ${PORT}`));
